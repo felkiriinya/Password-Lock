6756f4f0d1c5ce3fbd113d0f8027672a9ae5762c
felkiriinya <felkiriinya@gmail.com>
2020-10-10 18:09:03 +0300

    added the user class

---
+++ user.py
@@ -0,0 +1,28 @@
+class User
+     
+     '''
+     Generating new instances of users using this class
+     '''
+
+     users_list = []
+
+    def __init__(self, username, password):
+
+        '''
+        __init__ method helps define our objects properties
+
+        Args:
+            username: login's username for a new user
+            password: login:s password for a new user
+        '''
+
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+
+        '''
+        Method used to save user objects into the users list
+        '''
+
+        User.users_list.append(self)
