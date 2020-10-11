f70c2ed3dd4836b2546b8011f52c280afdb5e358
felkiriinya <felkiriinya@gmail.com>
2020-10-11 11:04:55 +0300

    allowing the user to create a new password locker account

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 from user import User
 from socials import Credentials
 import random
+import string
 
 def create_user(username,password):
     '''
@@ -24,14 +25,50 @@ def create_social_account(accountname,username,password):
     new_account= Credentials(account_name,username,password)
     return new_account
 
-def delete_social_account(socials)
+def delete_social_account(socials):
     '''
     function to delete an account
     '''
     socials.delete_account()          
 
-def display_accounts()
+def display_accounts():
     '''
     Function that returns all the saved 
     '''
     return Credentials.display_accounts()
+
+def main():
+    print("WELCOME TO YOUR PERSONAL PASSWORD LOCKER")
+    print("What is your name?")
+    print('\n')
+    user_name= input()
+    print('\n')
+    print(f"Hello {user_name}! You can use these short codes to navigate through")
+    print('\n')
+
+    # while True:
+
+        print('cc - Create a new password locker account')
+        print('lg - Log in into your created account')
+        print('ex - Exit your password locker account')
+
+        short_code = input().lower()
+
+        if short_code == 'cc':
+            print('Create your password locker account')
+            print("-"*10)
+
+            print('Username...')
+            username = input()
+
+            print('Password...')
+            password = input()
+
+            save_new_user(create_user(username,password))
+
+            print ('\n')
+            print('Congratulations ')
+
+if __name__ == '__main__':
+
+    main()
