4101e2a92782019f78e278d88b232599fec4f4b2
felkiriinya <felkiriinya@gmail.com>
2020-10-11 14:07:05 +0300

    added function that generates one password

--- run.py
+++ run.py
@@ -25,6 +25,12 @@ def create_social_account(accountname,username,password):
     new_account= Credentials(account_name,username,password)
     return new_account
 
+def save_social_account(socials):
+    '''
+    function to save a social account's credentials
+    '''
+    socials.save_account()
+
 def delete_social_account(socials):
     '''
     function to delete an account
@@ -36,6 +42,15 @@ def display_accounts():
     Function that returns all the saved 
     '''
     return Credentials.display_accounts()
+   
+def create_password(length=6):
+
+    '''
+    function that generates a password for you
+    '''
+    characters = string.ascii_letters +string.digits + string.punctuation
+    return ''.join(random.choice(characters) for i in range(length))
+
 
 def main():
     print("WELCOME TO YOUR PERSONAL PASSWORD LOCKER")
@@ -51,6 +66,7 @@ def main():
         print('cc - Create a new password locker account')
         print('lg - Log in into your created account')
         print('ex - Exit your password locker account')
+        print('\n')
 
         short_code = input().lower()
 
@@ -90,6 +106,7 @@ def main():
                  print ('\n')
                  print(f'Hello {login_username}. Welcome to your password locker account!')
                  print ('\n')
+                 social_account()
 
         elif short_code == 'lg':
              
@@ -105,6 +122,7 @@ def main():
                 else:
                     print(f'Hello {lg_username}. Welcome to your password locker account!')
                     print ('\n')
+                    social_account()
         elif short_code == 'ex':
 
                 print('Bye! You can come back later!')
@@ -112,7 +130,45 @@ def main():
         else:
                 print('Wrong short code! Try again')
 
+
+def social_account():
+    print('Now you can store your social accounts credentials here!')
+    print ('\n')
+    print('Use these short codes to navigate through')
+    print ('\n')
+
+    while True:
+            print('asc - Add a new social account credentials')
+            print('esc - Add an existing social account credentials')
+            print('dsc - Display your available social accounts credentials')
+            print('del - Delete a saved account credentials')
+            print('ex - Exit from the account')
+
+            short_code = input().lower()
+
+            if short_code == 'asc':
+                print('Add a new social account credentials')
+                print("-"*10)
+
+                print('Social Account Name ...')
+                accountname = input()
+
+                print('Username....')
+                username= input()
                 
+                print ('\n')
+                print('Use:')
+                print('gp - To get the password generated for you')
+                print('cp - To create your own password')
+
+                short_code1 = input().lower()
+                if short_code1 = 'gp':
+
+                save_social_account(create_social_account(accountname,username,password))
+
+
+
+
 if __name__ == '__main__':
 
     main()
